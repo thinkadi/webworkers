a5513cfc860c30964d7a07924be35be81a022ab8
thinkadi <thinkadi@outlook.com>
2016-04-15 11:36:32 -0700

    Create InsertMillionNumbers.js

---
+++ 201604/InsertMillionNumbers.js
@@ -0,0 +1,15 @@
+function insertMillionNumbers() {
+    for (i = 0; i < 1000; i++) {
+        for (j = 0; j < 1000; j++) {
+            for (k = 0; k < 1000; k++) {
+                db.numbers.insert({
+                    a: i,
+                    b: j,
+                    c: k
+                });
+            }
+        }
+    }
+}
+
+insertMillionNumbers();
