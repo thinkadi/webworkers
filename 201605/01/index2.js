e6cf7ed11157bcfc0084bad878082d8302ab304c
Adi Challa <thinkadi@outlook.com>
2016-05-20 16:40:27 -0700

    Status Code

--- 201605/01/index2.js
+++ 201605/01/index2.js
@@ -3,6 +3,7 @@ var server = http.createServer(function (req, res) {
     var body = "Hello World";
     res.setHeader("Content-Length", body.length);
     res.setHeader("Content-Type", "text/plain");
+    res.statusCode = 200;
     res.end(body);
 });
 server.listen(3000, function () {
