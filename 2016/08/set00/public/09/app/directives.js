39e88c8b4818cc4b8a57d5784bbb6d406e7a0fb2
thinkadi <thinkadi@outlook.com>
2016-08-20 14:47:29 -0700

    Web Workers

---
+++ 2016/08/set00/public/09/app/directives.js
@@ -0,0 +1,30 @@
+app.directive("navBar", function () {
+    return {
+        templateUrl: 'app/templates/nav-bar.html'
+    };
+});
+
+app.directive("home", function () {
+    return {
+        templateUrl: 'app/templates/home.html'
+    };
+});
+
+app.directive("register", function () {
+    return {
+        templateUrl: 'app/templates/register.html',
+        controller: 'registerController'
+    };
+});
+
+app.directive("login", function () {
+    return {
+        templateUrl: 'app/templates/login.html'
+    };
+});
+
+app.directive("myList", function () {
+    return {
+        templateUrl: 'app/templates/my-list.html'
+    };
+});
