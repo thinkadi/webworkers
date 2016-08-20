db5e519d1e8e434527e1a6054d1c6a7c720c987e
thinkadi <thinkadi@outlook.com>
2016-08-19 17:21:17 -0700

    Nav Bar Links

---
+++ 2016/08/public/set01/06/app/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('shoppingListApp', ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'app/views/home.html'
+    });
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/login', {
+        templateUrl: 'app/views/login.html'
+    });
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/my-list', {
+        templateUrl: 'app/views/my-list.html'
+    });
+});
