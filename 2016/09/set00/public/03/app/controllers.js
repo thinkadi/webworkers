3e24bce7675d115db504507130b44f2bfe8be58f
thinkadi <thinkadi@outlook.com>
2016-09-16 13:01:23 -0700

    Web Workers

---
+++ 2016/09/set00/public/03/app/controllers.js
@@ -0,0 +1,27 @@
+app.controller('registerController', ['$rootScope', '$scope', '$location', 'authService', function ($rootScope, $scope, $location, authService) {
+    $scope.registerUser = {};
+    $scope.error = {};
+    $scope.register = function () {
+        authService.register($scope.registerUser)
+            .then(function (response) {
+                $location.path("/");
+            }, function (errorMessage) {
+                $scope.error.message = errorMessage;
+            });
+    };
+    }]);
+
+app.controller('loginController', ['$rootScope', '$scope', '$location', 'authService', function ($rootScope, $scope, $location, authService) {
+    $scope.loginUser = {};
+    $scope.error = {};
+    $scope.login = function () {
+        authService.login($scope.loginUser)
+            .then(function (response) {
+                $rootScope.user = {};
+                $rootScope.user.loggedIn = true;
+                $location.path("/");
+            }, function (errorMessage) {
+                $scope.error.message = errorMessage;
+            });
+    };
+    }]);
