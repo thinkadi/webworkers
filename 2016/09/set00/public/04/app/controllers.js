7823f7358e054345d63c9ed4e8008e55e0ddb5eb
thinkadi <thinkadi@outlook.com>
2016-09-16 13:09:52 -0700

    Web Workers

---
+++ 2016/09/set00/public/04/app/controllers.js
@@ -0,0 +1,37 @@
+app.controller('registerController', ['$rootScope', '$scope', '$location', 'authService', function ($rootScope, $scope, $location, authService) {
+    $scope.registerUser = {};
+    $scope.error = {};
+    $scope.register = function () {
+        authService.register($scope.registerUser)
+            .then(function (response) {
+                $location.path("/");
+            }, function (errorMessage) {
+                $scope.error.message = errorMessage;
+            });
+    };
+    }]);
+
+app.controller('loginController', ['$rootScope', '$scope', '$location', 'authService', function ($rootScope, $scope, $location, authService) {
+    $scope.loginUser = {};
+    $scope.error = {};
+    $scope.login = function () {
+        authService.login($scope.loginUser)
+            .then(function (response) {
+                $rootScope.user = {};
+                $rootScope.user.loggedIn = true;
+                $location.path("/");
+            }, function (errorMessage) {
+                $scope.error.message = errorMessage;
+            });
+    };
+    }]);
+
+app.controller('navBarController', ['$rootScope', '$scope', '$location', 'authService', function ($rootScope, $scope, $location, authService) {
+    $scope.logout = function () {
+        authService.logout()
+            .then(function (response) {
+                $rootScope.user.loggedIn = false;
+                $location.path("/");
+            });
+    };
+    }]);
