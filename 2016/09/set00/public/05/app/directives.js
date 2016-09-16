b226ad3df74dc70dc562b3a7bdd8cd1fd7101991
thinkadi <thinkadi@outlook.com>
2016-09-16 13:48:00 -0700

    Web Workers

---
+++ 2016/09/set00/public/05/app/directives.js
@@ -0,0 +1,32 @@
+app.directive("navBar", function () {
+    return {
+        templateUrl: 'app/templates/nav-bar.html',
+        controller: 'navBarController'
+    };
+});
+
+app.directive("home", function () {
+    return {
+        templateUrl: 'app/templates/home.html'
+    };
+});
+
+app.directive("register", function () {
+    return {
+        templateUrl: 'app/templates/register.html',
+        controller: 'registerController'
+    };
+});
+
+app.directive("login", function () {
+    return {
+        templateUrl: 'app/templates/login.html',
+        controller: 'loginController'
+    };
+});
+
+app.directive("myList", function () {
+    return {
+        templateUrl: 'app/templates/my-list.html'
+    };
+});
---
+++ 2016/09/set00/public/05/app/routes.js
@@ -0,0 +1,27 @@
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'app/views/home.html'
+    });
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/register', {
+        templateUrl: 'app/views/register.html'
+    });
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/login', {
+        templateUrl: 'app/views/login.html'
+    });
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/my-list', {
+        templateUrl: 'app/views/my-list.html'
+    });
+});
