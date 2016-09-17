5f121fedc48e226a35e03e486955b9d5f44f1b94
thinkadi <thinkadi@outlook.com>
2016-09-17 10:26:32 -0700

    Web Workers

---
+++ 2016/09/set00/public/07/app/controllers.js
@@ -0,0 +1,50 @@
+app.controller('registerController', ['$rootScope', '$scope', '$location', 'authService', 'usersService', function ($rootScope, $scope, $location, authService, usersService) {
+    $scope.registerUser = {};
+    $scope.error = {};
+    $scope.register = function () {
+        authService.register($scope.registerUser)
+            .then(function (response) {
+                var loginUser = {};
+                loginUser.email = $scope.registerUser.email;
+                loginUser.password = $scope.registerUser.password;
+                authService.login(loginUser)
+                    .then(function (response) {
+                        usersService.loadUserInfo()
+                            .then(function (response) {
+                                $rootScope.user = usersService.getUser();
+                                $location.path("/");
+                            });
+                    });
+            }, function (errorMessage) {
+                $scope.error.message = errorMessage;
+            });
+    };
+    }]);
+
+app.controller('loginController', ['$rootScope', '$scope', '$location', 'authService', 'usersService', function ($rootScope, $scope, $location, authService, usersService) {
+    $scope.loginUser = {};
+    $scope.error = {};
+    $scope.login = function () {
+        authService.login($scope.loginUser)
+            .then(function (response) {
+                usersService.loadUserInfo()
+                    .then(function (response) {
+                        $rootScope.user = usersService.getUser();
+                        $location.path("/");
+                    });
+            }, function (errorMessage) {
+                $scope.error.message = errorMessage;
+            });
+    };
+    }]);
+
+app.controller('navBarController', ['$rootScope', '$scope', '$location', 'authService', 'usersService', function ($rootScope, $scope, $location, authService, usersService) {
+
+    $rootScope.user = usersService.getUser();
+
+    $scope.logout = function () {
+        authService.logout();
+        $rootScope.user = usersService.getUser();
+        $location.path("/");
+    };
+    }]);
