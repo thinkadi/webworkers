5fde888af9870f694252ea3dc2aa14fc5cae7687
thinkadi <thinkadi@outlook.com>
2016-09-17 08:43:52 -0700

    Web Workers

---
+++ 2016/09/set00/public/07/app/controllers.js
@@ -0,0 +1,50 @@
+app.controller('registerController', ['$rootScope', '$scope', '$location', 'authService', function ($rootScope, $scope, $location, authService) {
+    $scope.registerUser = {};
+    $scope.error = {};
+    $scope.register = function () {
+        authService.register($scope.registerUser)
+            .then(function (response) {
+                var loginUser = {};
+                loginUser.email = $scope.registerUser.email;
+                loginUser.password = $scope.registerUser.password;
+                authService.login(loginUser)
+                    .then(function (response) {
+                        authService.loadUserInfo()
+                            .then(function (response) {
+                                $rootScope.user = authService.getUser();
+                                $location.path("/");
+                            });
+                    });
+            }, function (errorMessage) {
+                $scope.error.message = errorMessage;
+            });
+    };
+    }]);
+
+app.controller('loginController', ['$rootScope', '$scope', '$location', 'authService', function ($rootScope, $scope, $location, authService) {
+    $scope.loginUser = {};
+    $scope.error = {};
+    $scope.login = function () {
+        authService.login($scope.loginUser)
+            .then(function (response) {
+                authService.loadUserInfo()
+                    .then(function (response) {
+                        $rootScope.user = authService.getUser();
+                        $location.path("/");
+                    });
+            }, function (errorMessage) {
+                $scope.error.message = errorMessage;
+            });
+    };
+    }]);
+
+app.controller('navBarController', ['$rootScope', '$scope', '$location', 'authService', function ($rootScope, $scope, $location, authService) {
+
+    $rootScope.user = authService.getUser();
+
+    $scope.logout = function () {
+        authService.logout();
+        $rootScope.user = authService.getUser();
+        $location.path("/");
+    };
+    }]);
---
+++ 2016/09/set00/public/07/app/services.js
@@ -0,0 +1,95 @@
+app.service('authService', ['$http', '$q', function ($http, $q) {
+
+    var user = {};
+
+    var userString = localStorage.getItem("userString");
+    if (userString) {
+        user = JSON.parse(userString);
+    }
+
+    var authUrl = "/auth";
+    var apiUrl = "/api"
+    var usersUrl = apiUrl + "/users";
+
+    this.register = function (registerUser) {
+        var deferred = $q.defer();
+        if (!registerUser.email) {
+            deferred.reject("Email cannot be blank");
+        } else if (!registerUser.password) {
+            deferred.reject("Password cannot be blank");
+        } else if (!registerUser.name) {
+            deferred.reject("Please enter your First Name and Last Name");
+        } else if (!registerUser.name.first) {
+            deferred.reject("First Name cannot be blank");
+        } else if (!registerUser.name.last) {
+            deferred.reject("Last Name cannot be blank");
+        } else {
+            var registerUrl = authUrl + "/register";
+            $http.post(registerUrl, registerUser)
+                .success(function (response) {
+                    deferred.resolve(response);
+                })
+                .error(function (err, status) {
+                    deferred.reject(err);
+                });
+        }
+        return deferred.promise;
+    };
+
+    this.login = function (loginUser) {
+        var deferred = $q.defer();
+        if (!loginUser.email) {
+            deferred.reject("Email cannot be blank");
+        } else if (!loginUser.password) {
+            deferred.reject("Password cannot be blank");
+        } else {
+            var registerUrl = authUrl + "/bearer-token";
+            $http.post(registerUrl, loginUser)
+                .success(function (response) {
+                    deferred.resolve(response);
+                    user.loggedIn = true;
+                    user.bearerToken = response.bearerToken;
+                    localStorage.setItem("userString", JSON.stringify(user));
+                })
+                .error(function (err, status) {
+                    deferred.reject(err);
+                });
+        }
+        return deferred.promise;
+    };
+
+    this.logout = function () {
+        var deferred = $q.defer();
+        localStorage.removeItem("userString");
+        user = {};
+        deferred.resolve("User logged out successfully");
+        return deferred.promise;
+    };
+
+    this.getUser = function () {
+        return user;
+    };
+
+    this.loadUserInfo = function () {
+        var deferred = $q.defer();
+        var thisUserUrl = usersUrl + "/me";
+        $http({
+                method: 'GET',
+                url: thisUserUrl,
+                headers: {
+                    "Authorization": "Bearer" + " " + user.bearerToken.token
+                }
+            })
+            .success(function (response) {
+                deferred.resolve(response);
+                user.email = response.email;
+                user.name = response.name;
+                localStorage.setItem("userString", JSON.stringify(user));
+            })
+            .error(function (err, status) {
+                deferred.reject(err);
+            });
+        return deferred.promise;
+    };
+
+}]);
